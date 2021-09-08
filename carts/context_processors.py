748796f1af6ce93656106da752d58f77e6c6315d
Aathithyan123 <Techhunter113@gmail.com>
2021-09-08 20:49:05 +0530

    first commit

---
+++ carts/context_processors.py
@@ -0,0 +1,21 @@
+from .models import Cart, CartItem
+from carts.views import _cart_id
+
+def counter(request):
+    cart_count=0
+    if 'admin' in request.path:
+        return {}
+    else:
+        try:
+            cart = Cart.objects.filter(cart_id = _cart_id(request))
+            if request.user.is_authenticated:
+                cart_itmes = CartItem.objects.all().filter(user = request.user)
+            else:
+                cart_itmes = CartItem.objects.all().filter(cart = cart[:1])
+            for cart_item in cart_itmes:
+                cart_count += cart_item.quantity
+
+        except Cart.DoesNotExist:
+            cart_count = 0
+        
+    return dict(cart_count = cart_count)
